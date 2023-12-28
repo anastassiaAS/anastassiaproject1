f179a393b337308b517cdbfb9eff415a1478fb4e
SamsungiphonePro <>
2023-12-28 16:39:28 +0200

    version01

---
+++ Anastassia.fungame/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_1 : NPC {
+	 
+	// Use this for initialization
+	public override void OnSetUpDialogue() {
+		Speech.AddDialogue("0", "Welcome to this magical world!", "1");
+		Speech.AddDialogue("1", "Your first task will be to collect all coins", "2");
+		Speech.AddDialogue("2", "Your second task will be to collect double jump skills", "3");
+		Speech.AddDialogue("3", "Only after completing these tasks you will be able to go to the next level", "4");
+		Speech.AddDialogue("4", "You will have 3 levels", "5");
+		Speech.AddDialogue("5", "Good luck!");
+
+	}
+
+	// Triggered when the player comes in range of the NPC
+	public override void OnTriggerNPC( Collider other ){
+		Speech.SetDialogue("0");
+	}
+}
---
+++ Anastassia.fungame/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public int coins;
+    protected void Start()
+    {
+        coins = 0;
+    }
+    protected void OnTriggerEnter(Collider collided)
+    {
+        if (collided.gameObject.tag == "Coin")
+        {
+            Destroy(collided.gameObject);
+            if (coins == 0)
+            {
+                HUD.Message("First coin, good job!");
+            }
+            else
+            {
+                HUD.Message("Here is your second one!");
+            }
+            coins++;
+            HUD.UpdateCoinCount(coins);
+        }
+
+        if (collided.gameObject.name == "Powerup Object 1")
+        {
+            Destroy(collided.gameObject);
+            Abilities.doubleJumpEnabled = true;
+            HUD.Message("A double jump is possible!");
+        }
+    }
+}
